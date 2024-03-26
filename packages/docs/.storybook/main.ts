a2733d855d116ebe94c015a0d3c5bb851c7af42a
Iago Novaes <iagonovaes19@gmail.com>
2024-03-25 23:11:12 -0300

    adc-tokens

---
+++ packages/docs/.storybook/main.ts
@@ -0,0 +1,32 @@
+import { dirname, join } from "path";
+module.exports = {
+  "stories": [
+    "../src/stories/**/*.mdx",
+    "../src/pages/**/*.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
+
+  "addons": [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-mdx-gfm")
+  ],
+
+  "framework": {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {}
+  },
+
+  "features": {
+    "storyStoreV7": true
+  },
+
+  docs: {
+    autodocs: true
+  }
+}
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
